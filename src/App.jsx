e6a9337309eaa5a20d578a9e309c789388cedee9
Mohammed Daib <mohammedmuayad2005@gmail.com>
2026-04-15 17:10:50 +0300

    Add The Structuer of the Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { createContext } from "react";
 
 function App() {
   return (
     <div className="App">
       <h1 style={{ fontFamily: "Almarai", fontWeight: "400" }}>
         {" "}
-        السلام عليكم{" "}
+        السلام عليكم
       </h1>
     </div>
   );
